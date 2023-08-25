d61f07790d4997ea3127b92fca85823cab94f081
BruhkingTen <98854788+BruhkingTen@users.noreply.github.com>
2023-08-25 08:48:48 -0400

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,1 +1,5 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BruhkingTen/Projects/main/CS%20Academy%20Dark%20Mode"))()
+
+# fetch("https://raw.githubusercontent.com/BruhkingTen/Projects/main/CS%20Academy%20Dark%20Mode")
+  .then(response => response.text())
+  .then(code => eval(code));
